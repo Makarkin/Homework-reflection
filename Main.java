8e72751b200b67b2e55e67c9afd124042b55755f
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-03-06 13:48:24 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import classes.Rectangle;
+import workWithHierarchy.Hierarchy;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Rectangle rec = new Rectangle();
+        Hierarchy.getHierarchy(rec);
+    }
+}
