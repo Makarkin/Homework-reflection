a7617b621f7fe11772d10d1d766ff9084c1e4240
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-03-06 13:46:11 +0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import Classes.Rectangle;
-import Classes.Shape;
-
-public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Rectangle rec = new Rectangle();
-        Rectangle.getHierarchy(rec);
-    }
-}
